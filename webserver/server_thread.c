000982352be06072be3be6af751db596a0afb356
Naveed Ashfaq <naveed.ashfaq@mail.utoronto.ca>
2019-10-25 12:07:47 -0400

    Initial code for Lab 4

---
+++ webserver/server_thread.c
@@ -0,0 +1,115 @@
+#include "request.h"
+#include "server_thread.h"
+#include "common.h"
+
+struct server {
+	int nr_threads;
+	int max_requests;
+	int max_cache_size;
+	int exiting;
+	/* add any other parameters you need */
+};
+
+/* static functions */
+
+/* initialize file data */
+static struct file_data *
+file_data_init(void)
+{
+	struct file_data *data;
+
+	data = Malloc(sizeof(struct file_data));
+	data->file_name = NULL;
+	data->file_buf = NULL;
+	data->file_size = 0;
+	return data;
+}
+
+/* free all file data */
+static void
+file_data_free(struct file_data *data)
+{
+	free(data->file_name);
+	free(data->file_buf);
+	free(data);
+}
+
+static void
+do_server_request(struct server *sv, int connfd)
+{
+	int ret;
+	struct request *rq;
+	struct file_data *data;
+
+	data = file_data_init();
+
+	/* fill data->file_name with name of the file being requested */
+	rq = request_init(connfd, data);
+	if (!rq) {
+		file_data_free(data);
+		return;
+	}
+	/* read file, 
+	 * fills data->file_buf with the file contents,
+	 * data->file_size with file size. */
+	ret = request_readfile(rq);
+	if (ret == 0) { /* couldn't read file */
+		goto out;
+	}
+	/* send file to client */
+	request_sendfile(rq);
+out:
+	request_destroy(rq);
+	file_data_free(data);
+}
+
+/* entry point functions */
+
+struct server *
+server_init(int nr_threads, int max_requests, int max_cache_size)
+{
+	struct server *sv;
+
+	sv = Malloc(sizeof(struct server));
+	sv->nr_threads = nr_threads;
+	sv->max_requests = max_requests;
+	sv->max_cache_size = max_cache_size;
+	sv->exiting = 0;
+	
+	if (nr_threads > 0 || max_requests > 0 || max_cache_size > 0) {
+		TBD();
+	}
+
+	/* Lab 4: create queue of max_request size when max_requests > 0 */
+
+	/* Lab 5: init server cache and limit its size to max_cache_size */
+
+	/* Lab 4: create worker threads when nr_threads > 0 */
+
+	return sv;
+}
+
+void
+server_request(struct server *sv, int connfd)
+{
+	if (sv->nr_threads == 0) { /* no worker threads */
+		do_server_request(sv, connfd);
+	} else {
+		/*  Save the relevant info in a buffer and have one of the
+		 *  worker threads do the work. */
+		TBD();
+	}
+}
+
+void
+server_exit(struct server *sv)
+{
+	/* when using one or more worker threads, use sv->exiting to indicate to
+	 * these threads that the server is exiting. make sure to call
+	 * pthread_join in this function so that the main server thread waits
+	 * for all the worker threads to exit before exiting. */
+	sv->exiting = 1;
+
+	/* make sure to free any allocated resources */
+	free(sv);
+}
