d9b81a4da32fdeeb405408c53e0e09c63fcd9c43
Naveed Ashfaq <naveed.ashfaq@mail.utoronto.ca>
2019-11-12 18:34:16 -0500

    Added cache and LRU functions

--- webserver/server_thread.c
+++ webserver/server_thread.c
@@ -2,6 +2,44 @@
 #include "server_thread.h"
 #include "common.h"
 
+// Hash Function for hash table
+// Hash function used is djb2, obtained from
+// http://www.cse.yorku.ca/~oz/hash.html
+unsigned long
+hash(char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash;
+}
+
+typedef struct cache_entry {
+	struct file_data *data;
+	struct cache_entry *next;
+} CacheEntry;
+
+typedef struct cache {
+	CacheEntry *table;
+	long size;
+	long capacity;
+	long max_cache_size;
+} Cache;
+
+typedef struct lru_ele {
+	CacheEntry *entry;
+	struct lru_ele *next;
+} LRUEntry;
+
+typedef struct lru {
+	LRUEntry *head;
+	LRUEntry *tail;
+	int size;
+} LRUList;
+
 struct request_buffer {
 	int* requests;
 	int in;
@@ -21,7 +59,180 @@ struct server {
 	pthread_cond_t cv_full;
 	pthread_cond_t cv_empty;
 	pthread_mutex_t lock;
+
+	Cache *cache;
+	LRUList *LRU;
 };
+
+//	=================	LRU Functions	=================	//
+
+LRUEntry* find_in_LRU(LRUList *LRU, CacheEntry *target) {
+	LRUEntry* current = LRU->head;
+	while (current != NULL) {
+		if (current->entry == target)
+			break;
+		current = current->next;
+	}
+	return current;
+}
+
+LRUEntry* find_in_LRU_with_prev(LRUList *LRU, CacheEntry *target, CacheEntry **prev) {
+	*prev = NULL;
+	LRUEntry* current = LRU->head;
+	while (current != NULL) {
+		if (current->entry == target)
+			break;
+		*prev = current;
+		current = current->next;
+	}
+	return current;
+}
+
+// Appends given node to queue
+void add_to_LRU(LRUList *LRU, CacheEntry *entry) {
+	LRUEntry *node = (LRUEntry *) malloc(sizeof(LRUEntry));
+	assert(node);
+	node->entry = entry;
+	node->next = NULL;
+	if (LRU->head == NULL) {
+		// LRU is currently empty
+		LRU->head = node;
+		LRU->tail = node;
+	} else {
+		LRU->tail->next = node;	// Append to end of queue
+		LRU->tail = LRU->tail->next;	// Move tail to new end of queue
+	}
+	LRU->size++;	// Update queue size
+}
+
+// Moves the head of the queue to the end
+void move_head_to_end(LRUList *LRU) {
+	if (LRU->size <= 1)
+		return;
+	LRUEntry *old_head = LRU->head;
+	LRU->head = LRU->head->next;
+	LRU->tail->next = old_head;
+	LRU->tail = old_head;
+	old_head->next = NULL;
+}
+
+void move_node_to_end(LRUList *LRU, CacheEntry *entry) {
+	if (LRU->size <= 1)
+		return;
+	LRUEntry *prev;
+	LRUEntry *node = find_in_LRU_with_prev(LRU, entry, &prev);
+	if (!node)
+		return;
+	if (node == LRU->head)
+		move_head_to_end(LRU);
+	else if (node != LRU->tail) {
+		prev->next = node->next;
+		node->next = NULL;
+		LRU->tail->next = node;
+		LRU->tail = node;
+	}
+}
+
+// Removes and returns and the head of the queue
+LRUEntry* pop_LRU(LRUList *LRU) {
+	LRUEntry* old_head = LRU->head;
+	LRU->head = LRU->head->next;
+	old_head->next = NULL;
+	LRU->size--;	// Update queue size
+	return old_head;
+}
+
+// Removes target node from queue
+void remove_from_LRU(LRUList *LRU, CacheEntry *target) {
+	if (LRU->size == 1) {
+		// This is the only node in the queue
+		free(LRU->head);
+		LRU->head = NULL;
+		LRU->tail = NULL;
+		LRU->size = 0;
+		return;
+	}
+
+	LRUEntry *current = LRU->head;
+	LRUEntry *prev = NULL;
+	while (current != NULL) {
+		if (current->entry == target)
+			break;
+		prev = current;
+		current = current->next;
+	}
+
+	if (current) {
+		if (!prev)
+			LRU->head = current->next;
+		else
+			prev->next = current->next;
+		current->next = NULL;
+		if (current == LRU->tail)
+			LRU->tail = prev;
+
+		free(current);
+		LRU->size--;
+	}
+}
+
+// Frees all nodes in the given queue
+void clear_LRU(LRUList *LRU) {
+	if (LRU == NULL)
+		return;
+	while (LRU->head != NULL) {
+		LRUEntry *next = LRU->head->next;
+		free(LRU->head);
+		LRU->head = next;
+	}
+	LRU->tail = NULL;
+	LRU->size = 0;
+}
+
+//	=================	End of LRU Functions		=================	//
+
+
+// ======================== Hashtable Operations ========================
+
+struct file_data *cache_lookup(Cache *cache, char *filename) {
+	// TODO: Apply mutex
+	unsigned long key = hash(filename) % cache->capacity;
+	CacheEntry *entry = &cache->table[key];
+	while (entry->next != NULL) {
+		entry = entry->next;
+		if (!strcmp(entry->data->file_name, filename)) {
+			// TODO: Update LRU List
+			return entry->data;		// Cache hit
+		}
+	}
+	return NULL;	// Cache miss
+}
+
+int cache_insert(Cache *cache, struct file_data *file) {
+	// TODO: Apply mutex
+	// TODO: Check for space limitations
+	// Assume doesn't exist in cache already
+	unsigned long key = hash(file->file_name) % cache->capacity;
+	CacheEntry *entry = &cache->table[key];
+	while (entry->next != NULL) {
+		entry = entry->next;
+	}
+
+	entry->next = (CacheEntry *) malloc(sizeof(CacheEntry));
+	entry = entry->next;
+	entry->data = file;
+	entry->next = NULL;
+	cache->size += file->file_size;
+
+	// TODO: Update LRU List
+	return 1;
+}
+
+void cache_evict(Cache *cache, unsigned long amount_to_evict) {
+	// TODO: Implement this function
+}
+
+// ======================== End of Hashtable Operations ========================
 
 /* static functions */
 
@@ -50,7 +261,7 @@ file_data_free(struct file_data *data)
 static void
 do_server_request(struct server *sv, int connfd)
 {
-	int ret;
+	int ret, cache_inserted = 0;
 	struct request *rq;
 	struct file_data *data;
 
@@ -62,18 +273,32 @@ do_server_request(struct server *sv, int connfd)
 		file_data_free(data);
 		return;
 	}
-	/* read file, 
-	 * fills data->file_buf with the file contents,
-	 * data->file_size with file size. */
-	ret = request_readfile(rq);
-	if (ret == 0) { /* couldn't read file */
-		goto out;
+
+	// Check for cache hit
+	struct file_data *cache_value = cache_lookup(sv->cache, data->file_name);
+	if (cache_value) {
+		file_data_free(data);
+		request_set_data(rq, cache_value);
+	} else {
+		/* read file, 
+		* fills data->file_buf with the file contents,
+		* data->file_size with file size. */
+		ret = request_readfile(rq);
+		if (ret == 0) { /* couldn't read file */
+			goto out;
+		} else {
+			// Add file to cache
+			cache_inserted = cache_insert(sv->cache, data);
+		}
 	}
+
+	
 	/* send file to client */
 	request_sendfile(rq);
 out:
 	request_destroy(rq);
-	file_data_free(data);
+	if (!cache_inserted)
+		file_data_free(data);
 }
 
 /* entry point functions */
@@ -177,14 +402,20 @@ server_init(int nr_threads, int max_requests, int max_cache_size)
 	}
 
 	if (max_cache_size > 0) {
-		// TODO in Lab 5
+		sv->cache = (Cache *) malloc(sizeof(Cache));
+		assert(sv->cache);
+		sv->cache->max_cache_size = max_cache_size;
+		sv->cache->capacity = 300;
+		sv->cache->table = (CacheEntry *) calloc(sv->cache->capacity * sizeof(CacheEntry));
+		assert(sv->cache->table);
+		sv->cache->size = 0;
+
+		sv->LRU = (LRUList *) malloc(sizeof(LRUList));
+		assert(sv->LRU);
+		sv->LRU->head = NULL;
+		sv->LRU->tail = NULL;
+		sv->LRU->size = 0;
 	}
-
-	/* Lab 4: create queue of max_request size when max_requests > 0 */
-
-	/* Lab 5: init server cache and limit its size to max_cache_size */
-
-	/* Lab 4: create worker threads when nr_threads > 0 */
 
 	return sv;
 }
